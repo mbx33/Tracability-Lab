84347157c359166b57d56aa5c97bbd3a58035a90
Matt Baxter <mbx33@hotmail.com>
2021-08-17 12:16:07 -0600

    added rollbar

--- Server/index.js
+++ Server/index.js
@@ -7,9 +7,20 @@ const port = process.env.PORT || 4040;
 
 app.use(express.json());
 
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: 'b1bf4ee9e6194090a7a8bd4b7bc5d55b',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
 app.get('/', function(req, res) {
+    rollbar.log("Hello world!");
     res.sendFile(path.join(__dirname, '../client/index.html'))
 });
+
+
+
 
 
 
